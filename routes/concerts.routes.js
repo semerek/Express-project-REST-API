8e8de2cfdc775e61730e86cfb2b2b10e284a8f96
Marta Semrau <mm.semrau@gmail.com>
2020-12-04 17:21:24 +0100

    Add mongo database, controllers

--- routes/concerts.routes.js
+++ routes/concerts.routes.js
@@ -1,51 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
 
-const app = express()
+const ConcertController = require('../controllers/concerts.controller');
 
-
-//CONCERTS
-router.route('/concerts').get((req, res) => {
-    res.json(db.concerts);
-});
-
-router.route('/concerts/:id').get((req, res) => {
-    res.json(db.concerts.find(concert => concert.id == req.params.id));
-});
-
-router.route('/concerts').post((req, res) => {
-    const {performer, genre, price, day} = req.body;
-    const id =  db.concerts[Math.floor(Math.random() * db.concerts.length)];
-   
-    db.concerts.push({ id, performer, genre, price, day });
-    res.json({ message: 'OK' });
-
-});
-
-router.route('/concerts/:id').put((req, res) => {
-    
-    const concerts = db.concerts.find(concert => concert.id == req.params.id);
-
-    if(!concerts) {
-        res.status(404).json({message:'Not found'});
-        return;
-    }
-
-    concerts.performer = req.body.performer;
-    concerts.genre = req.body.genre;
-    concerts.price = req.body.price;
-    concerts.day = req.body.day;
-    concerts.info = req.body.info;
-
-    res.json({message: 'OK'});
-});
-
-router.route('/concerts/:id').delete((req, res) => {
-    db.concerts = db.concerts.filter(concert => concert.id != req.params.id);
-
-     res.json({message:'OK'});
-});
-
+router.get('/concerts', ConcertController.getAll);
+router.get('/concerts/:id', ConcertController.getById);
+router.post('/concerts', ConcertController.addNew);
+router.put('concerts/:id', ConcertController.updateById);
+router.delete('concerts/:id', ConcertController.deleteById);
 
 module.exports = router;
--- routes/seats.routes.js
+++ routes/seats.routes.js
@@ -1,69 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../db.js');
-const { v4: uuidv4 } = require('uuid');
 
+const SeatController = require('../controllers/seats.controller');
 
-
-const app = express()
-
-
-//SEATS
-
-router.route('/seats').get((req, res) => {
-    res.json(db.seats);
-});
-
-router.route('/seats/:id').get((req, res) => {
-    res.json(db.seats.find(seat => seat.id == req.params.id));
-});
-
-router.route('/seats').post((req, res) => {
-    
-    const { day, seat, client, email } = req.body;
-
-    const checkBooking = db.seats.some(booking => booking.day == day && booking.seat == seat);
-    if (!checkBooking) {
-        db.seats.push({
-            id: uuidv4(),
-            day: day,
-            seat: seat,
-            client: client,
-            email: email,
-        });
-        req.io.emit('seatsUpdated', db.seats);
-        res.json({ message: 'ok' });
-    } else {
-        res.status(409).json({ message: "The slot is already taken..." });
-    }
-});
-
-router.route('/seats/:id').put((req, res) => {
-    
-    const{day, seat, client, email} = req.body;
-    const seats = db.seats.find(seat => seat.id == req.params.id);
-    index = db.seats.indexOf(seats);
-
-    const updatedSeat= {
-        ...seats,
-        day: day,
-        seat: seat,
-        client: client,
-        email: email,
-    };
-    
-    db.seat(index) = updatedSeat;
-    res.json({ message: 'OK'});
-    
-});
-
-router.route('/seats/:id').delete((req, res) => {
-    const seat = db.seats.find(seat => seat.id == req.params.id);
-    const index = db.seat.indexOf(seat);
-    db.seats.splice(index, 1);
-    
-    res.json({ message:'OK' });
-});
-
+router.get('/seats', SeatController.getAll);
+router.get('/seats/:id', SeatController.getById);
+router.post('/seats', SeatController.addNew);
+router.put('seats/:id', SeatController.updateById);
+router.delete('seats/:id', SeatController.deleteById);
 
 module.exports = router;
